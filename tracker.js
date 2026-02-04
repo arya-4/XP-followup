2de5c435fee9fce640db15131db1b023364c30b8
arya-4 <124735001+arya-4@users.noreply.github.com>
2026-02-04 14:50:38 -0800

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -1,3 +1,9 @@
 <!-- need one section for the reminder tab, one for the input tab --> 
 
   <!-- basic syntax =  element.addEventListener(event, function, options); --> 
+
+
+<!--element.addEventListener(button, function, options);-->
+
+  const Sx_date = new Date (input)
+
